aae42629f38f83be2be1d523692bf4051232bc3b
alonfa <alondrazita@gmail.com>
2021-12-07 17:44:19 +0100

    Optimize repeated lines check
    This commit removes extra checks in the repeated lines function, reducing the overall time.

--- toothpick_fractal/repeated.m
+++ toothpick_fractal/repeated.m
@@ -1,11 +1,11 @@
 % returns 1 if the point is repeated (lines overlap)
-function repeated = repeated(step, Lines, nA, t1)
+function repeated = repeated(step, lines_per_iter, nA, t1)
 
 global MainA
 
 repeated = 0;
 for n1 = 1:step
-    for a1=1:Lines(1);%lines_per_iter(n1)
+    for a1=1:lines_per_iter(n1)*2
         t2=[MainA(a1,1,n1),MainA(a1,2,n1)];
         if ((t1==t2)&((a1~=nA)|(step~=n1)))
             repeated=1;
