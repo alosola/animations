58a8918701c8f63204e06d70eee1c16dea3983e4
alonfa <alondrazita@gmail.com>
2021-12-07 17:28:52 +0100

    Add animation capability
    This commit adds the animation capability to the toothpick fractal graph, and saves the result as a gif file in the same folder as the code.

--- toothpick_fractal/main.m
+++ toothpick_fractal/main.m
@@ -7,10 +7,21 @@ global MACounter
 
 MainA = [1 0; -1 0];
 
-iter = 48; % number of fractal iterations
+iter = 50; % number of fractal iterations
 loops = iter-1; % number of loops calculated
 lines_per_iter = zeros(1,iter);
 lines_per_iter(1) = 1;
+
+%% Initialize graph
+colour = hsv(iter); % colourmap for the fractal image
+imsize = 5;
+figure_toothpick = figure(1);
+title('Toothpick fractal')
+im = cell(1,loops);
+xlim([-imsize,imsize])
+ylim([-imsize,imsize])
+hold on
+
 
 %% Calculate each iteration
 for step=1:loops
@@ -36,34 +47,42 @@ for step=1:loops
                 addxy(0,1,nA,step);
                 addxy(0,-1,nA,step);
             end
+
         end
     end
-
     % Register lines added in this iteration
     lines_per_iter(step+1) = (MACounter - 1) / 2;
+
+    %% Plot lines from this iteration
+    for nA=1:lines_per_iter(step)
+        plot([MainA(nA*2-1,1,step),MainA(nA*2,1,step)],[MainA(nA*2-1,2,step),MainA(nA*2,2,step)],'Color',colour(step,:));
+    end
+
+    if (mod(step,5)==0)
+        imsize = imsize + 2;
+        xlim([-imsize,imsize])
+        ylim([-imsize,imsize])
+    end
+    xlabel(sprintf('Iteration %i', step));
+    frame = getframe(figure_toothpick);
+    im{step} = frame2im(frame);
+
+    %%
 end
+
+%% Save image frames to gif
+
+delay = 2/loops; % gif will be 2 seconds long
+writegif('toothpick.gif',im,loops,delay);
+hold off
 
 
 %% Plot the number of lines per iteration
 
-figure(1);
+figure(2);
 hold on
 title('Number of lines added per iteration')
 plot(1+linspace(0,iter,iter),lines_per_iter,'.-')
 xlabel('iteration')
 ylabel('new lines')
-
-%% Plot the toothpick fractal
-
-figure(2);
-title('Toothpick fractal')
-hold on
-
-N=size(MainA,1)/2;
-colour = parula(iter);
-
-for j=1:iter
-    for i=1:lines_per_iter(j)
-        plot([MainA(i*2-1,1,j),MainA(i*2,1,j)],[MainA(i*2-1,2,j),MainA(i*2,2,j)],'Color',colour(j,:));
-    end
-end
+xlim([1,iter])
