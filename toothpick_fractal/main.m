2d7076006b665114a835c1975054344b5332d4fc
alonfa <alondrazita@gmail.com>
2021-12-08 19:03:12 +0100

    Modify animation plot functionality
    This commit adds a viewing screen for the running code, so that the iterations can be monitored as the program calculates the fractal.

--- toothpick_fractal/main.m
+++ toothpick_fractal/main.m
@@ -5,7 +5,7 @@ clear all
 global MainA
 global MACounter
 
-iter = 60; % number of fractal iterations
+iter = 100; % number of fractal iterations
 loops = iter-1; % number of loops calculated
 
 MainA = zeros(400,2,iter);
@@ -13,6 +13,8 @@ MainA = [1 0; -1 0];
 
 lines_per_iter = zeros(1,iter);
 lines_per_iter(1) = 1;
+
+times_per_iter = zeros(1,iter);
 
 % ignorable after a box is closed
 boxClose = zeros(1,20);
@@ -27,40 +29,67 @@ maxy=-1;
 % set to 1 for animated gif, set to 0 for static graph of fractal
 animation = 1;
 
-% set to 1 for plot of lines per iteration
-plot_lines_per_iteration = 0;
-
 % select colour scheme for the fractal image
 % select a number between 1 and 19
-colour = select_color(3,iter);
+colour = select_color(5,iter);
 
 figure_toothpick = figure(1);
-title('Toothpick fractal')
-hold on
-box on
+figure_toothpick.WindowState = 'maximized';
 
 if animation
     duration = 4; % duration of gif in seconds
     looped_gif = 1; % 1 for looped gif
     imsize = 5;
+
+    % fractal
+    subplot(2,2,[1,3]);
+    title('Toothpick fractal')
+    hold on
+    box on
     xlim([-imsize,imsize])
     ylim([-imsize,imsize])
+
+    % lines
+    subplot(2,2,2);
+    hold on
+    title('Number of lines added per iteration')
+    xlabel('iteration')
+    ylabel('new lines')
+%     xlim([1,iter])
+    box on
+
+    % initialize the frame collection
+    im = cell(1,loops);
+
+
+    % time
+    subplot(2,2,4);
+    hold on
+    title('Time per iteration')
+    xlabel('iteration')
+    ylabel('time spent ms')
+%     xlim([1,iter])
+    box on
 
     % initialize the frame collection
     im = cell(1,loops);
 end
 
+% start timer
+tic
 
 %% Calculate each iteration
 for step=1:loops
 
     % Calculate lines for this iteration
     lines_per_iter = toothpick_iteration(step, lines_per_iter, maxx, maxy, BCC, ignoreable);
+    times_per_iter(step) = toc;
 
     if animation
-        [im, imsize] = plot_iteration(step, lines_per_iter, imsize, im, colour, figure_toothpick);
+        [im, imsize] = plot_iteration(step, lines_per_iter, times_per_iter, imsize, im, colour, figure_toothpick);
+    else
+        disp(step)
     end
-
 end
 
 
@@ -80,12 +109,12 @@ end
 
 %% Plot the number of lines per iteration
 
-if plot_lines_per_iteration
-    figure(2);
-    hold on
-    title('Number of lines added per iteration')
-    plot(1+linspace(0,iter,iter),lines_per_iter,'.-','LineWidth',2)
-    xlabel('iteration')
-    ylabel('new lines')
-    xlim([1,iter])
-end
+% if plot_lines_per_iteration
+%     figure(2);
+%     hold on
+%     title('Number of lines added per iteration')
+%     plot(1+linspace(0,iter,iter),lines_per_iter,'.-','LineWidth',2)
+%     xlabel('iteration')
+%     ylabel('new lines')
+%     xlim([1,iter])
+% end
--- toothpick_fractal/plot_iteration.m
+++ toothpick_fractal/plot_iteration.m
@@ -1,11 +1,13 @@
-function [im, imsize] = plot_iteration(step, lines_per_iter, imsize, im, colour, figure_toothpick)
+function [im, imsize] = plot_iteration(step, lines_per_iter, times_per_iter, imsize, im, colour, figure_toothpick)
 
 global MainA
 
+% plot fractal
+subplot(2,2,[1,3]);
 plot_lines(step, lines_per_iter, colour)
 
 if (mod(step,10)==0)
-    imsize = max((imsize + 5),max(MainA,[],'all'));
+    imsize = max([(imsize + 5),(imsize + step/10),max(MainA,[],'all')]);
     xlim([-imsize,imsize])
     ylim([-imsize,imsize])
 end
@@ -13,3 +15,21 @@ end
 xlabel(sprintf('Iteration %i', step));
 frame = getframe(figure_toothpick);
 im{step} = frame2im(frame);
+
+
+
+if (step ~= 1)
+    % plot lines per iteration
+    subplot(2,2,2);
+    if ((lines_per_iter(step-1) == 4) && (step > 5))
+        xline(step,':')
+    end
+    plot([step,step+1],[lines_per_iter(step-1),lines_per_iter(step)],'.-','LineWidth',1.5,'MarkerSize',10,'Color',colour(step,:))
+
+    % plot time per iteration
+    subplot(2,2,4);
+    if ((lines_per_iter(step-1) == 4) && (step > 5))
+        xline(step,':')
+    end
+    plot([step,step+1],[times_per_iter(step-1),times_per_iter(step)],'b.-','LineWidth',1.5,'MarkerSize',10,'Color',colour(step,:))
+end
